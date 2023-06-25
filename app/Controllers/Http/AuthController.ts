9e56adc738a6d90e8fa7f26cf411dc56950959a1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-25 09:50:44 +0700

    feat: register token

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,26 +1,30 @@
+import { inject } from '@adonisjs/fold'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
+import AuthService from 'App/Services/AuthService'
 import LoginValidator from 'App/Validators/Auth/LoginValidator'
 import RegisterValidator from 'App/Validators/Auth/RegisterValidator'
 
+@inject()
 export default class AuthController {
+    constructor(private authService: AuthService) {}
+
     public async login(context: HttpContextContract) {
         await context.request.validate(LoginValidator)
 
         const { email, password } = context.request.body()
-        const token = await context.auth.use('api').attempt(email, password)     
 
-        return context.response.ok({
-            token: token.toJSON(),
-            user: token.user
-        })
+        return context.response.ok(await this.authService.login({
+            credentials: { email, password },
+            context
+        }))
     }
 
     public async register(context: HttpContextContract) {
         await context.request.validate(RegisterValidator)
 
-        const user = await User.create(context.request.except(['password_confirmation']))
-
-        return context.response.created(user)
+        return context.response.ok(await this.authService.register({
+            user: context.request.except(['password_confirmation']),
+            context
+        }))
     }
 }
