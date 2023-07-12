2872c86a53516f5845fbfd9628c49ff07f649b09
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-07-12 11:00:27 +0800

    feat: validate task status color

--- app/Validators/TaskStatus/CreateTaskStatusValidator.ts
+++ app/Validators/TaskStatus/CreateTaskStatusValidator.ts
@@ -1,13 +1,13 @@
 import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
 export default class CreateTaskStatusValidator {
     constructor(protected ctx: HttpContextContract) {}
 
     public schema = schema.create({
         name: schema.string({}, [
             rules.required(),
-        ])
+        ]),
+        color: schema.enum.nullableAndOptional(['light', 'dark', 'primary', 'success', 'warning', 'danger'])
     })
 
     public messages: CustomMessages = {}
