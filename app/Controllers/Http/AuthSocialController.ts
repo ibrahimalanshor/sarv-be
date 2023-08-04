82e65ecf6d1c4fc7c22afcf6972ea0ee06146ba7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-08-04 10:15:13 +0800

    feat: google login validate

--- app/Controllers/Http/AuthSocialController.ts
+++ app/Controllers/Http/AuthSocialController.ts
@@ -1,12 +1,15 @@
 import { inject } from '@adonisjs/fold'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { AuthService } from 'App/Services/AuthService'
+import GoogleLoginValidator from 'App/Validators/AuthSocial/GoogleLoginValidator'
 
 @inject()
 export default class AuthSocialController {
     constructor(private authService: AuthService) {}
 
     public async google(context: HttpContextContract) {
+        await context.request.validate(GoogleLoginValidator)
+
         const user = await context.ally.use('google').userFromToken(context.request.body().token)
         const res = await this.authService.loginSocial({
             user: {
